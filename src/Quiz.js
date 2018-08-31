c7fcdecce72f9b52bf938554008974d08e46cb88
Mathieu GYORFFY <mathieu.gyorffy@thalesdigital.io>
2018-08-31 14:26:47 +0200

    Creating a QuizQuestionButton Component

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,10 +16,7 @@ class Quiz extends Component {
     render() {
         return (
             <div>
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
-                {/* <div className="QuizQuestion">
-                    {quizData.quiz_questions[1].instruction_text}
-                </div> */}
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
             </div>
         )
     }
